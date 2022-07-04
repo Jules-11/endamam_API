3de9c2be7b276bca0b75d057b31c18d918792ee7
Jules-11 <julien.galeuzzi@gmail.com>
2022-07-04 10:29:33 +0000

    update

--- src/js/component/Recipe.jsx
+++ src/js/component/Recipe.jsx
@@ -3,16 +3,21 @@ import React from "react";
 const Recipe = ({ item }) => {
   const nutrientsArr = ["CA", "CHOLE", "FAT", "FIBTG", "SUGAR", "NA", "FE"];
   return (
-    <div className="card" style={{ maxWidth: "500px", marginTop: "40px" }}>
+    <div className="card" style={{ maxWidth: "350px", marginTop: "40px" }}>
+        <div style={{paddingTop: "20px"}}>
+        <h2 className="card-title">{item.recipe.label}</h2>
+        </div>
       <img
         src={item.recipe.image}
         alt={item.recipe.label}
         style={{ padding: "20px" }}
       />
       <div className="card-body">
-        <h5 className="card-title">{item.recipe.label}</h5>
+        
         <div className="card-text fw-light">
-          <strong>Nutrients:</strong>
+        <div className="text-center">
+        <h3>Nutrients</h3>
+        </div>
           <ul>
             {nutrientsArr.map((nut) => {
               return (
@@ -27,17 +32,29 @@ const Recipe = ({ item }) => {
             })}
           </ul>
         </div>
-        <strong>Ingredients:</strong>
-        <ul className="list-group list-group-flush">
+        <br/>
+        <div className="text-center">
+        <h3>Ingredients</h3>
+        </div>
           {item.recipe.ingredientLines.map((ing) => (
-            <li className="list-group-item">{ing}</li>
+            <div className="custom-control custom-checkbox list-group-item">
+            <label className="custom-control-label " for="customCheck1">{ing}</label>
+            <div className="d-flex justify-content-end">
+            <input type="checkbox" className="custom-control-input" id="customCheck1"/>
+            </div>
+          </div>
+            
           ))}
-        </ul>
-        <hr />
-        <div className="card-body">
+          
+        <div className="card-body d-flex justify-content-center">
           <button type="button" className="btn btn-dark">
             <a href={item.recipe.url} target="_blank" className="card-link text-white">
               Full Recipe!
+            </a>
+          </button>
+          <button type="button" className="btn btn-dark">
+            <a href="#" className="card-link text-white">
+              Add To List
             </a>
           </button>
         </div>
