67189836762240337f022086619c976b16233821
Jules-11 <julien.galeuzzi@gmail.com>
2022-07-04 15:25:48 +0000

    search bar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Recipe from "./Recipe.jsx";
 
 
@@ -8,20 +8,28 @@ const Home = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [searchInput, setSearchInput] = useState("chicken");
+  const searchRef = useRef();
 
-  
+
+
+  const searchBarHandler = (e) => {
+    // console.log("Coucou", searchRef.current.value)
+    e.preventDefault();
+    setSearchInput(searchRef.current.value)
+  };
   
 
   useEffect(() => {
     fetch(
-      "https://api.edamam.com/api/recipes/v2?type=public&app_id=e5010e00&app_key=0326e037783040d1e8513857ee63d982&q=chicken"
+      "https://api.edamam.com/api/recipes/v2?type=public&app_id=e5010e00&app_key=0326e037783040d1e8513857ee63d982&q=" + searchInput
     )
       .then((response) => response.json())
       .then(
         (result) => {
           setIsLoaded(true);
           setItems(result.hits);
-          console.table(result.hits[0].recipe);
+          // console.table(result.hits[0].recipe);
         },
         (error) => {
           setIsLoaded(true);
@@ -29,7 +37,9 @@ const Home = () => {
           console.log("nooooooooo", error);
         }
       );
-  }, []);
+  }, [searchInput]);
+
+  
 
   if (error) {
     return <>{error.message}</>;
@@ -39,6 +49,14 @@ const Home = () => {
     console.table(items);
     return (
       <div>
+        <div className="row mt-5">
+        <div className="col-6 offset-4">
+          <form onSubmit={searchBarHandler}>
+          <input type="text" ref={searchRef}></input>
+          <button type="sumbit">Search</button>
+          </form>
+        </div>
+        </div>
         <ul>
           {items.map((item) => (
             
